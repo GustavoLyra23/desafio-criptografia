b75167a96cddf94e96517f9a77c8abea712a8868
GustavoLyra23 <gustavolyra23@gmail.com>
2024-07-26 23:25:31 -0300

    added UserInformation entity

---
+++ src/main/java/com/criptografia/demo/entities/UserInformation.java
@@ -0,0 +1,73 @@
+package com.criptografia.demo.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user_information")
+public class UserInformation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userDocument;
+    private String creditCardToken;
+    private Long userValue;
+
+    public UserInformation() {
+    }
+
+    public UserInformation(Long id, String userDocument, String creditCardToken, Long userValue) {
+        this.id = id;
+        this.userDocument = userDocument;
+        this.creditCardToken = creditCardToken;
+        this.userValue = userValue;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserDocument() {
+        return userDocument;
+    }
+
+    public void setUserDocument(String userDocument) {
+        this.userDocument = userDocument;
+    }
+
+    public String getCreditCardToken() {
+        return creditCardToken;
+    }
+
+    public void setCreditCardToken(String creditCardToken) {
+        this.creditCardToken = creditCardToken;
+    }
+
+    public Long getUserValue() {
+        return userValue;
+    }
+
+    public void setUserValue(Long userValue) {
+        this.userValue = userValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserInformation that = (UserInformation) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
